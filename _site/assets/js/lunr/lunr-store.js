24cf6645c54cd434f85387ca2f5f46ca9d7b6a9c
richpin <richpinkr99@gmail.com>
2021-09-02 02:15:49 +0900

    Commit the rest

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "jekyll serve 명령 실행 시 crash-abort 이슈",
+        "excerpt":"이제는 나도 개발 블로그를 하나 해야겠다는 생각으로 Github Pages로 지금 이 블로그를 만들기 시작하던 중이었다. ###jekyll 테마를 다운받아 로컬 호스팅 해보기 많은 개발자들이 그러하듯 jekyll 테마를 이용하기 위해 homebrew로 버전 관리자인 rbenv 먼저 설치해주고 $ brew install rbenv rbenv로 가능한 Ruby버전들을 확인해본다. $ rbenv install --list 2.6.8 2.7.4 3.0.2 jruby-9.2.19.0...","categories": [],
+        "tags": [],
+        "url": "/crash-abort-post/",
+        "teaser": null
+      }]
